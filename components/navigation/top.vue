d818384c784546d5872eea73ef18701f2aab63c9
Eduardo López <loama18@gmail.com>
2022-12-09 18:45:24 -0600

    update app icon

--- components/navigation/top.vue
+++ components/navigation/top.vue
@@ -1,5 +1,5 @@
 <template>
   <div id="navbar-top" class="border-zinc-200 bg-white border-b box-content fixed left-0 text-center top-0 w-screen">
-    <img class="h-4 inline-block mt-4" src="~/assets/img/almond.png" alt="almond logo" />
+    <img class="h-4 inline-block mt-5" src="~/assets/img/almond.png" alt="almond logo" />
   </div>
 </template>
